686c53740c2652d616332c8b4bb7b6339478c16d
ryandvl <contatoryandvl55@gmail.com>
2023-11-09 20:02:22 -0300

    .

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -14,12 +14,12 @@ export const metadata: Metadata = {
     description: 'testando, e por que ainda ta o link?',
     images: [
       {
-        url: 'https://ryandvl.github.io/opengraph-image.png',
+        url: 'https://cdn.discordapp.com/attachments/1141103894961868973/1170485957712154695/image.png?ex=655936fe&is=6546c1fe&hm=d9d727ac13aeb39ede4224c016453aa6b57ed841dcdd18ecba8f40cdbd199e36&',
         width: 800,
         height: 600,
       },
       {
-        url: 'https://ryandvl.github.io/opengraph-image.png',
+        url: 'https://cdn.discordapp.com/attachments/1141103894961868973/1170485957712154695/image.png?ex=655936fe&is=6546c1fe&hm=d9d727ac13aeb39ede4224c016453aa6b57ed841dcdd18ecba8f40cdbd199e36&',
         width: 1800,
         height: 1600,
         alt: 'ata',
